c69bc4064bff1eef294b4933254df7f839ea677f
unale <96617967+unale@users.noreply.github.com>
2026-06-08 20:15:27 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = 'tourniED-v3';
+const FILES = ['/', '/index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => clients.claim())
+  );
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      if (cached) return cached;
+      return fetch(e.request).then(response => {
+        caches.open(CACHE).then(cache => cache.put(e.request, response.clone()));
+        return response;
+      }).catch(() => caches.match('/index.html'));
+    })
+  );
+});
